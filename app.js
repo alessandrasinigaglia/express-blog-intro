7c360cbb5f086dcb3bef27a7d146d3e2f941b5cf
alessandrasinigaglia <alessandra.sinigaglia06@gmail.com>
2025-01-21 16:20:19 +0100

    feat: add base project

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
-console.log("test")
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('Server del mio blog')
+})
+
+app.get('/foto', (req, res) => {
+  res.json([])
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
